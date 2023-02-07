d29ac463c35db330b4c2070efcb38a285617b0b3
aarevaloe <aarevalo@duppla.co>
2023-02-06 19:26:57 -0500

    Ajustes diseño y adición de navbar en home

--- src/componentes/pages/Home.js
+++ src/componentes/pages/Home.js
@@ -5,7 +5,13 @@ import Iperfil from "../../img/iconoperfil.png"
 import BarraProgreso from "../../img/barraprogreso.png"
 import Istateg from "../../img/Istateg.png"
 import Idupplaverdeblanco from "../../img/Idupplaverdeblanco.png"
+import Vperfil from "../../img/vperfil.svg"
+import Voferta from "../../img/voferta.svg"
+import Vinmueble from "../../img/vinmueble.svg"
+import Vdocs from "../../img/vdocs.svg"
+import Vlogout from "../../img/vlogout.svg"
 import { Link } from "react-router-dom";
+
 
 
 
@@ -51,14 +57,10 @@ function Home() {
       .then(response => response.json())
       .then(response => setData(response))
       .catch(err => console.error(err));
-
 */}
-
-
   return (
     <div className=" container-fluid ">
       <div className="container-sm">
-
         {/*Contenedor de perfil */}
         <div className="profile container-fluid">
           <div className="row contenedor-img-duppla">
@@ -67,82 +69,188 @@ function Home() {
           <div className="col-4 ">
             <Link to='/profile' className="link-styles"> <img src={Iperfil}
               className="img-fluid  img-user"
-              alt="perfil" />
+              alt="perfil"/>
             </Link>
           </div><hr />
-          <div className="col-8">
+          <div className="col-6">
             <div className="card-body">
               <h5 className="card-title card-home text-white" >{data.nombre}</h5>
               <p className="text-orange">{fecha}</p>
             </div>
           </div>
-        </div>
+          <nav className=" col-2 navbar  ">
+            <div className="container-fluid ">
+              <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span className="navbar-toggler-icon navbar-dark"></span>
+              </button>
+              <div className="offcanvas offcanvas-bottom " tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div className="offcanvas-body navbar-container">
+                  <ul className="navbar-nav " >
+                    <li className="nav-item">
+                      <Link to="/profile">
+                        <div className="row ">
+                          <div className="col-8 outline ">
+                            <div className="row">
+                              <div className="card-state-properties nav-link active">
+                                <div className="card-body col-1  img-state-propety">
+                                  <img src={Vperfil} className="" alt="" height='24px' width='24px' />
+                                </div>
+                                <div className="col-10 outline">
+                                  <p className=" text-docs"><b >Perfil </b></p>
+                                </div>
+                              </div>
 
-        {/*Contenedor de oferta */}
-        <div className="Container-cards-seccion-m centrado container-fluid" id="cardComponet">
-        <Link to='/offer' className="link-style">
-          <div className="card-seccion ">
-            <div className="row ">
-              <div className="col-1">
-                <img src={Idocumento} className="img-icono-card" alt="" />
-              </div>
-              <div className="col-4 card-home-offer">
-                <h4 className="card-title card-top"><b>Ultima oferta</b></h4>
-                < p className="link-style">Abrir</p>
-              </div>
-              <div className="col-1">
-                <img src={Istateg} className="btn-state-home" alt="" height='12px' width='12px' />
-              </div>
-              <div className="col-4">
-                <div className="card-body">
-                  <p className="card-text">03/02/2023</p>
-                  <p className="card-text-aprov">{data.estado_oferta}</p>
+
+                            </div>
+
+
+                          </div>
+                        </div>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/offer">
+                        <div className="row ">
+                          <div className="col-8 outline ">
+                            <div className="row">
+                              <div className="card-state-properties nav-link active">
+                                <div className="card-body col-1  img-state-propety">
+                                  <img src={Voferta} className="" alt="" height='24px' width='24px' />
+                                </div>
+                                <div className="col-10 outline">
+                                  <p className=" text-docs"><b >Ultima oferta</b></p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/property">
+                        <div className="row ">
+                          <div className="col-8 outline ">
+                            <div className="row">
+                              <div className="card-state-properties nav-link active">
+                                <div className="card-body col-1  img-state-propety">
+                                  <img src={Vinmueble} className="" alt="" height='24px' width='24px' />
+                                </div>
+                                <div className="col-10 outline">
+                                  <p className=" text-docs"><b >Inmueble</b></p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/documents">
+                        <div className="row ">
+                          <div className="col-8 outline ">
+                            <div className="row">
+                              <div className="card-state-properties nav-link active">
+                                <div className="card-body col-1  img-state-propety">
+                                  <img src={Vdocs} className="" alt="" height='24px' width='24px' />
+                                </div>
+                                <div className="col-10 outline">
+                                  <p className=" text-docs"><b>Documentos</b></p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="">
+                        <div className="row ">
+                          <div className="col-8 outline ">
+                            <div className="row">
+                              <div className="card-state-properties nav-link active">
+                                <div className="card-body col-1  img-state-propety">
+                                  <img src={Vlogout} className="" alt="" height='24px' width='24px' />
+                                </div>
+                                <div className="col-10 outline">
+                                  <p className=" text-docs"><b >Cerrar sesión</b></p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
-          </div>
+          </nav>
+        </div>
+        {/*Contenedor de oferta */}
+        <div className="Container-cards-seccion-m centrado " id="cardComponet">
+          <Link to='/offer' className="link-style">
+            <div className="card-seccion ">
+              <div className="row ">
+                <div className="col-1">
+                  <img src={Idocumento} className="img-icono-card" alt="" />
+                </div>
+                <div className="col-4 card-home-offer">
+                  <h4 className="card-title card-top"><b>Ultima oferta</b></h4>
+                  < p className="link-style">Abrir</p>
+                </div>
+                <div className="col-1">
+                  <img src={Istateg} className="btn-state-home" alt="" height='12px' width='12px' />
+                </div>
+                <div className="col-4">
+                  <div className="card-body">
+                    <p className="card-text">03/02/2023</p>
+                    <p className="card-text-aprov">{data.estado_oferta}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </Link>
 
         </div>
         {/*Contenedor de inmueble */}
-        <div className="Container-cards-seccion centrado container-sm" id="cardComponet">
-        <Link to='/property' className="link-style">
-          <div className="card-seccion"  >
-            <div className="row ">
-              <div className="col-1">
-                <img src={Iinmueble} className="img-icono-card " alt="" />
-              </div>
-              <div className="col-4 card-home-offer">
-                <h4 className="card-title card-top "><b>Inmueble</b></h4>
-                <p className="link-style">Abrir</p>
-              </div>
-              <div className="col-1">
-                <img src={Istateg} className="btn-state-home" alt="" height='12px' width='12px' />
-              </div>
-              <div className="col-4">
-                <div className="card-body">
-                  <p className="card-text more">03/02/2023</p>
-                  <p className="card-text-aprov">{data.estado_inm}</p>
+        <div className="Container-cards-seccion-m centrado " id="cardComponet">
+          <Link to='/property' className="link-style">
+            <div className="card-seccion"  >
+              <div className="row ">
+                <div className="col-1">
+                  <img src={Iinmueble} className="img-icono-card " alt="" />
+                </div>
+                <div className="col-4 card-home-offer">
+                  <h4 className="card-title card-top "><b>Inmueble</b></h4>
+                  <p className="link-style">Abrir</p>
+                </div>
+                <div className="col-1">
+                  <img src={Istateg} className="btn-state-home" alt="" height='12px' width='12px' />
+                </div>
+                <div className="col-4">
+                  <div className="card-body">
+                    <p className="card-text more">03/02/2023</p>
+                    <p className="card-text-aprov">{data.estado_inm}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
           </Link>
         </div>
         {/*Menú documentos*/}
         <Link to='/documents' className="links text-black">
-        <div className="card-docs  ">
-          <div className="card-body col-8 text-docs">
-            <b>Menú documentos</b>
+          <div className="card-docs  ">
+            <div className="card-body col-8 text-docs">
+              <b>Menú documentos</b>
+            </div>
+            <div className="col-2 outline">
+              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEgAAABICAYAAABV7bNHAAAAAXNSR0IArs4c6QAAAgFJREFUeF7t2z1KxEAYxvH/XkAQPIKgYGMvinoiSys/Kks7jyOK1hZroeARRMUbyMAGRNzsJEPeeQae1BOS/OaZN5PZ2Rk+egVm9ukXMNCKhBjIQGVFxAlygpygMgEnqMzPNcgJcoLKBJygMj/XIOEErQNfZf07/dm1EnQDHAEHwPv0jzn+CjWAroGTxS2/qiNFA10Bp3/6UxopEugcuFgSdlmkKKCUmpSevkMSKQIo1ZtUd3IOOaQIoBdgO0dn0Wa+eMN9DjhnsqYRQBvAI7A14CmegUOgOlIEUHJpFikKqFmkSKAmkaKBmkOqAdQh3QE76oW7FlBySV/zD+pINYFKkPaB7wHpG920NtBYpCfgOAJJAUgaSQVIFkkJSBJJDUgOSRGoQ7oFdge8fiYp3KpAa8C9gf6PhwxOuj21BKXZtcTQ6vpOCWjMp8ckded3sFWAJHFUhtgYnLQkG/I9VjtBY3HC1qtrAsnj1BxiaRHfC2ZLZsFN/cIRPcSawokeYs3hRAI1iRMF1CxOFNAbsDlg2SJNAtP2vI8B50zWNKJID93+sqewaSH6YzUHSW5vUNQQ6zrjDLhcMhYkcaKB0vW8iTOjWnobcAaSN5JnIPmvCBlI8k0i5kHyCH03aKAV3WcgA5WNcCfICXKCygScoDI/1yAnyAkqE3CCyvxcg1b4/QBf035JQzfVwQAAAABJRU5ErkJggg=="
+                className="arrow-menu" />
+
+            </div>
           </div>
-          <div className="col-2 outline">
-            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEgAAABICAYAAABV7bNHAAAAAXNSR0IArs4c6QAAAgFJREFUeF7t2z1KxEAYxvH/XkAQPIKgYGMvinoiSys/Kks7jyOK1hZroeARRMUbyMAGRNzsJEPeeQae1BOS/OaZN5PZ2Rk+egVm9ukXMNCKhBjIQGVFxAlygpygMgEnqMzPNcgJcoLKBJygMj/XIOEErQNfZf07/dm1EnQDHAEHwPv0jzn+CjWAroGTxS2/qiNFA10Bp3/6UxopEugcuFgSdlmkKKCUmpSevkMSKQIo1ZtUd3IOOaQIoBdgO0dn0Wa+eMN9DjhnsqYRQBvAI7A14CmegUOgOlIEUHJpFikKqFmkSKAmkaKBmkOqAdQh3QE76oW7FlBySV/zD+pINYFKkPaB7wHpG920NtBYpCfgOAJJAUgaSQVIFkkJSBJJDUgOSRGoQ7oFdge8fiYp3KpAa8C9gf6PhwxOuj21BKXZtcTQ6vpOCWjMp8ckded3sFWAJHFUhtgYnLQkG/I9VjtBY3HC1qtrAsnj1BxiaRHfC2ZLZsFN/cIRPcSawokeYs3hRAI1iRMF1CxOFNAbsDlg2SJNAtP2vI8B50zWNKJID93+sqewaSH6YzUHSW5vUNQQ6zrjDLhcMhYkcaKB0vW8iTOjWnobcAaSN5JnIPmvCBlI8k0i5kHyCH03aKAV3WcgA5WNcCfICXKCygScoDI/1yAnyAkqE3CCyvxcg1b4/QBf035JQzfVwQAAAABJRU5ErkJggg=="
-              className="arrow-menu" />
-           
-          </div>
-        </div>
         </Link>
-        
+
         {/*componente de estados*/}
         <div className="centrado  container-fluid">
           <div className=" row">
