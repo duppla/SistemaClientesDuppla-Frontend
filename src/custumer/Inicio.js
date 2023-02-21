16b687062959d540ce63ffb260dd8ea2540329dd
aarevaloe <aarevalo@duppla.co>
2023-02-20 21:49:46 -0500

    Se agrega navbar en inicio

--- src/custumer/Inicio.js
+++ src/custumer/Inicio.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 import './../custumer/inicio.css'
 import { Link } from 'react-router-dom';
@@ -8,11 +8,21 @@ import Istateb from "../../src/img/Istateb.png";
 import Istatem from "../../src/img/Istatem.png";
 import Istaten from "../../src/img/Istaten.png";
 import Iconpago from "../../src/img/Iconpago.png";
-import Iprogresbar from "../../src/img/Iprogresbar.png";
+
 import Igendacita from "../../src/img/Iagendacita.svg";
 import Iayuda from "../../src/img/Iayuda.svg";
 import Ihistorialpago from "../../src/img/Ihistorialpago.svg";
 import Imantenimiento from "../../src/img/Imantenimiento.svg";
+import Iperfilnavbar from "../../src/img/Iperfilnavbar.svg";
+import Ipagarnavbar from "../../src/img/Ipagarnavbar.svg";
+import Ihousenavbar from "../../src/img/Ihousenavbar.svg";
+import Ihistorialnavbar from "../../src/img/Ihistorialnavbar.svg";
+import Iayudanavbar from "../../src/img/Iayudanavbar.svg";
+
+
+
+
+
 
 
 import ProgressBar from './../componentes/pages/Logout';
@@ -74,25 +84,14 @@ function Inicio() {
                     <div className='title-init'>
                         <b><h1 className='title-init-progressbar  '>Actividad mensual</h1></b>
                     </div>
-
                     {/*componente de estados*/}
                     <div className="centrado  container-fluid">
-                        {/*<div className="row ">
-
-                            <img src={Iprogresbar} className="progres-img" alt="  " />
-
-
-    </div>*/}
-
                         {/*<div>
                             <ProgressBar value={value} maxValue={maxValue} color={color} />
                         </div>*/}
-
-                        
                         <div>
-                            <Progressbar  />
+                            <Progressbar />
                         </div>
-                   
                     </div>
                     {/*Pago mínimo */}
                     <div className="card-docs-init ">
@@ -153,17 +152,12 @@ function Inicio() {
 
                 </div>
             </div>
-
             {/*componente pago*/}
-
             <div className="btn btn-init-pago  centrado-btn  " >
                 <img src={Iconpago} className=" img-ingreso" id="btnIngresoFacebook" alt="ingreso google" width="32px" height="32px" />
                 <div className='text-white'>Pagar factura</div>
-
             </div>
-
             <hr />
-
             <div className='container-fluid container-btns'>
                 <div className="card-docs-init   ">
                     <div className=" col-3">
@@ -181,14 +175,48 @@ function Inicio() {
                     </div>
                     <div className="col-3 ">
                         <img src={Imantenimiento} className=" warning font-medium-2 mr-2" alt="" height='60px' width='60px' />
-                        <p>Mantenimien to </p>
+                        <p>Mantenimiento </p>
 
                     </div>
                 </div>
 
             </div>
 
+            <nav className="navbar bg-body-tertiary fixed-bottom">
+                <div className="container-fluid">
+                    <button className=" btn icon-navbar-custumer" type="button" data-bs-toggle="" data-bs-target="" >
+                        <Link to="/profile">
+                            <img src={Iperfilnavbar} className="navbar-toggler " alt="" />
+                        </Link>
+                    </button>
+                    <button className=" btn icon-navbar-custumer" type="button" data-bs-toggle="" data-bs-target="" >
+                        <Link to="/profile">
+                            <img src={Ipagarnavbar} className="navbar-toggler " alt="" />
+                        </Link>
+                    </button>
+                    <button className=" btn icon-navbar-custumer" type="button" data-bs-toggle="" data-bs-target="" >
+                        <Link to="/profile">
+                            <img src={Ihousenavbar} className="navbar-toggler " alt="" />
+                        </Link>
+                    </button>
+                    <button className=" btn icon-navbar-custumer" type="button" data-bs-toggle="" data-bs-target="" >
+                        <Link to="/profile">
+                            <img src={Ihistorialnavbar} className="navbar-toggler " alt="" />
+                        </Link>
+                    </button>
+                    <button className=" btn icon-navbar-custumer" type="button" data-bs-toggle="" data-bs-target="" >
+                        <Link to="/profile">
+                            <img src={Iayudanavbar} className="navbar-toggler " alt="" />
+                        </Link>
+                    </button>
 
+
+
+
+
+
+                </div>
+            </nav>
 
 
 
