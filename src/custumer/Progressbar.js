4884c774494927f32f86f009558ed26567cc813f
aarevaloe <aarevalo@duppla.co>
2023-02-21 22:58:04 -0500

    Desinstalación apexcharts

--- src/custumer/Progressbar.js
+++ src/custumer/Progressbar.js
@@ -1,28 +1,9 @@
 import React from 'react'
 import './../custumer/progressbar.css'
-import Chart from "react-apexcharts"
+
 
 function Progressbar() {
 
-
-     const options = {
-        chart: {
-          id: "basic-bar"
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-        }
-      }
-    
-    const series = [
-        {
-          name: "series-1",
-          data: [30, 40, 45, 50, 49, 60, 70, 91]
-        }
-      ]
-    
-
-     
 
   return (
 
